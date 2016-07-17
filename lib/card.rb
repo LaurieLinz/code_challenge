98a681974c169fa10051639772f7af8d47cf85ff
Laurie Linz <lvlinz@comcast.net>
2016-07-17 13:02:57 -0600

    Added logic.

--- lib/card.rb
+++ lib/card.rb
@@ -5,31 +5,22 @@ class Card
   SPADES = '♠'
   CLUBS = '♣'
 
-
-
   def initialize(suit, value)
-    @suit = suit
-    @value = value
+      @suit = suit
+      @value = value
   end
-
-
 
   def suit
     @suit
   end
 
-
-
   def value
     @value
   end
 
-
-
   def to_s
     "#{value} of #{suit}"
   end
-
 
   def inspect
     to_s
--- lib/deck.rb
+++ lib/deck.rb
@@ -10,8 +10,8 @@ class Deck
       Card::CLUBS,
       Card::HEARTS
     ].each do |suit|
-      ['A', 2, 3, 4, 5, 6, 7,
-        8, 9, 10, 'J', 'Q', 'K'].each do |value|
+      [2, 3, 4, 5, 6, 7,
+        8, 9, 10, 11, 12, 13, 14].each do |value| # jack = 11, queen = 12, king = 13, ace = 14 (high only)
         @cards.push(Card.new(suit, value))
       end
     end
@@ -24,24 +24,25 @@ class Deck
   def draw(number_of_cards=5)
     drawn_cards = []
     number_of_cards.times { drawn_cards.push(@cards.pop) }
-    drawn_cards
+
+    #drawn_cards.group_by {|a| a.value.to_s}
+    #drawn_cards.sort {|a,b| a.value.to_s <=> b.value.to_s}
+
+    #drawn_cards.sort {|a,b| a.value <=> b.value }.reverse
+    drawn_cards.group_by { |a| a.suit }
   end
-
-
 
   def shuffle
     @cards.shuffle!
   end
 
-
-
   def to_s
     "A deck of #{cards.count} cards"
   end
-
 
   def inspect
     to_s
   end
 
 end
+
