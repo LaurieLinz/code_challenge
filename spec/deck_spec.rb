b4b04a71a039c26793caa584ac8825e89b3d1be4
Laurie Linz <lvlinz@comcast.net>
2016-07-19 16:08:12 -0600

    Updated and wrote tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,14 +5,27 @@ require_relative '../lib/deck'
 deck = Deck.new
 deck.shuffle
 
+describe 'Deck and draw' do
+  describe Deck do
 
-describe Deck do
+    it 'Should be a deck of cards' do
 
-  it 'Should be a deck of cards' do
+      expect(deck.to_s).to eq('A deck of 52 cards')
 
-    expect(deck.to_s).to eq('A deck of 52 cards')
+    end
 
   end
 
-end
+  describe 'Draw' do
 
+    it 'Should be a draw of five cards' do
+
+      deck = Deck.new
+      deck.shuffle
+      deck.draw
+
+      expect(:draw).to  eq(:draw)
+
+    end
+  end
+end
---
+++ spec/hand_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+
+
+describe 'It should report the value of the hand' do
+
+  describe Hand do
+
+    it 'if the hand is a high card' do
+      it 'if the hand is a pair' do
+        it 'if the hand is two pair' do
+          it 'if the hand is  three of a kind' do
+            it 'if the hand is a straight' do
+              it 'if the hand is flush' do
+                it 'if the hand is a full house' do
+                  it 'if the hand is a four of a kind' do
+                    it 'if the hand is a straight flush' do
+                      it 'if the hand is a royla flush' do
+
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
